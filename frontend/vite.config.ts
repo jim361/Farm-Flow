5a2689d554ac7a6ecda3ec3c1b2fdb53777188c9
alaoq <150979605+alaoq@users.noreply.github.com>
2026-04-07 21:10:22 +0900

    fix logic builder react conflict

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import path from 'path' // 수정
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +9,11 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+// 수정
+resolve: {
+  alias: {
+    'react': path.resolve('./node_modules/react'),
+    'react-dom': path.resolve('./node_modules/react-dom'),
+  }
+}
 })
